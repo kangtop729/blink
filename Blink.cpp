006c456a31f7e3654d3ea1407a42b6ce926d5063
kangtop729 <37132916+kangtop729@users.noreply.github.com>
2018-03-08 20:19:15 +0900

    Update Blink.cpp

--- Blink.cpp
+++ Blink.cpp
@@ -1,19 +1,19 @@
 #include "Arduino.h"
 #include "Blink.h"
 
-Blink :: Blink(int pin)
+Blink::Blink(int pin)
 {
   pinMode(pin ,OUTPUT);
   _pin=pin;
 }
-void Blink ::on(int time)
+void Blink::on(int time)
 {
   
   digitalWrite(_pin,HIGH);
   delay(time);
 }
 
-void Blink :: off(int time)
+void Blink::off(int time)
 {
   digitalWrite(_pin,LOW);
   delay(time);
