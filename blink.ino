946eaa4f85da2faec9a2c2e1b9db4a8b2fafb00c
kangtop729 <37132916+kangtop729@users.noreply.github.com>
2018-03-08 20:17:55 +0900

    Update blink.ino

--- blink.ino
+++ blink.ino
@@ -1,1 +1,11 @@
+#include "Blink.h"
 
+Blink blink(LED_BUILTIN);
+void setup() {
+  Serial.begin(115200);
+  Serial.println("\nBlink Version 1.0 MinGoo Kang");
+}
+void loop(){
+  blink.on(500);
+  blink.off(300);
+}
