2bcff9cba14dbf9d40bd30ef61fa8970bf3138b1
kangtop729 <37132916+kangtop729@users.noreply.github.com>
2018-03-08 11:38:39 +0900

    Update Blink.h

--- Blink.h
+++ Blink.h
@@ -1,1 +1,13 @@
+#include <Blink.h>
+
+Blink blink(D4);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println(“\nBlinkVersion 1.0 Your_First_NameYour_Last_Name”);
+}
+void loop() {
+  blink.on(500);
+  blink.off(300);
+}
 
